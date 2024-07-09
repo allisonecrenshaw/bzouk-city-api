bd2d365a01e13750afec8c515c055ada5c092d41
Allison Crenshaw <allisonecrenshaw@gmail.com>
2024-07-09 12:05:04 -0500

    Adding src and test directories.

---
+++ src/utils/base.entity.ts
@@ -0,0 +1,21 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+
+export class BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @CreateDateColumn({ name: 'created_date' })
+  createdDate: Date;
+
+  @UpdateDateColumn({ name: 'updated_date' })
+  updatedDate: Date;
+
+  @DeleteDateColumn({ name: 'deleted_date' })
+  deletedDate: Date;
+}
---
+++ src/utils/config/data-source.ts
@@ -0,0 +1,21 @@
+import { DataSource } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+
+config();
+
+const configService = new ConfigService();
+
+export const dataSource = new DataSource({
+  type: 'postgres',
+  host: configService.get('DB_HOST'),
+  port: configService.get('DB_PORT'),
+  username: configService.get('DB_USERNAME'),
+  password: configService.get('DB_PASSWORD'),
+  database: configService.get('DB_NAME'),
+  synchronize: false,
+  logging: false,
+  entities: ['src/v1/**/*.entity.ts'],
+  migrations: ['src/migration/**/*.ts'],
+  subscribers: ['src/subscriber/**/*.ts'],
+});
