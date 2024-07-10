dbddb2776d96c01b32f112aacdf164ceaaf55e3e
Allison Crenshaw <allisonecrenshaw@gmail.com>
2024-07-10 14:06:55 -0500

    Tweaking orm config stuff again. Adding typeorm generate migration command to package.json.

--- src/utils/config/ormconfig.ts
+++ src/utils/config/ormconfig.ts
@@ -6,16 +6,23 @@ config();
 
 const configService = new ConfigService();
 
-export const dataSource = new DataSource({
+export default new DataSource({
   type: 'postgres',
-  host: configService.get('DB_HOST'),
-  port: configService.get('DB_PORT'),
-  username: configService.get('DB_USERNAME'),
-  password: configService.get('DB_PASSWORD'),
-  database: configService.get('DB_NAME'),
-  synchronize: false,
-  logging: false,
+  url: configService.get<string>('DB_URL'),
   entities: ['src/v1/**/*.entity.ts'],
   migrations: ['src/migrations/**/*.ts'],
-  subscribers: ['src/subscriber/**/*.ts'],
 });
+
+// export const dataSource = new DataSource({
+//   type: 'postgres',
+//   host: configService.get('DB_HOST'),
+//   port: configService.get('DB_PORT'),
+//   username: configService.get('DB_USERNAME'),
+//   password: configService.get('DB_PASSWORD'),
+//   database: configService.get('DB_NAME'),
+//   synchronize: false,
+//   logging: false,
+//   entities: ['src/v1/**/*.entity.ts'],
+//   migrations: ['src/migrations/**/*.ts'],
+//   subscribers: ['src/subscriber/**/*.ts'],
+// });
