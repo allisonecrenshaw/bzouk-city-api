0afe49dde7c52894d0efc2d11b66ab4a12d92959
Allison Crenshaw <allisonecrenshaw@gmail.com>
2024-07-10 15:19:52 -0500

    Adding link to Stack Overflow post that recommended tsx for ts file extension issue with ts-node.

--- notes.ts
+++ notes.ts
@@ -2,4 +2,6 @@
 Resources:
   - Configure Typeorm Migrations in 5 Minutes: 
     https://dev.to/andreasbergstrom/configure-typeorm-migrations-in-5-minutes-2njg 
+  - Stack Overflow article re: error for unable to read .ts file extension:
+    https://stackoverflow.com/questions/62096269/cant-run-my-node-js-typescript-project-typeerror-err-unknown-file-extension
 */
