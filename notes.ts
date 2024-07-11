ac74bb68a1c71692f81a6a3c5293a3f30e7bea18
Allison Crenshaw <allisonecrenshaw@gmail.com>
2024-07-11 15:11:19 -0500

    Adding note.

--- notes.ts
+++ notes.ts
@@ -4,4 +4,6 @@ Resources:
     https://dev.to/andreasbergstrom/configure-typeorm-migrations-in-5-minutes-2njg 
   - Stack Overflow article re: error for unable to read .ts file extension:
     https://stackoverflow.com/questions/62096269/cant-run-my-node-js-typescript-project-typeerror-err-unknown-file-extension
+  - NestJS Providers (Creating a Basic Service)
+    https://docs.nestjs.com/providers
 */
