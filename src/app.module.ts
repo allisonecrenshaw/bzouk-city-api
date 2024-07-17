00c4ecfc17348a63c2cec5d1361e9d1ebc695d41
Allison Crenshaw <allisonecrenshaw@gmail.com>
2024-07-17 15:32:17 -0500

    Adding .js extensions for now until I find a solution to the issue of modules not being found.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './app.controller.js';
+import { AppService } from './app.service.js';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
