002f08e00b01bb73cc0f15b8a7a04c693bc72a41
Allison Crenshaw <allisonecrenshaw@gmail.com>
2024-10-09 14:44:21 -0500

    Removing unnecessary code from main. Did not need this to access routes -- just forgot to do dependency injection in AppModule.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import * as dotenv from 'dotenv';
 import { ValidationPipe, Logger } from '@nestjs/common';
-import { ExpressAdapter } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const mainLogger = new Logger('Main');
@@ -11,34 +10,6 @@ async function bootstrap() {
 
   mainLogger.log('Calling await NestFactory.create(AppModule)');
   const app = await NestFactory.create(AppModule);
-
-  // Get routes
-  app.enableCors();
-  const httpAdapter = app.getHttpAdapter();
-  
-  if (httpAdapter instanceof ExpressAdapter) {
-    const server = httpAdapter.getInstance();
-
-    // Logging routes
-    const routeLogger = new Logger('Routes');
-    app
-      .getHttpAdapter()
-      .getInstance()
-      ._router.stack.forEach(
-        (layer: {
-          route: { methods: { [x: string]: unknown }; path: any };
-        }) => {
-          if (layer.route) {
-            const methods = Object.keys(layer.route.methods)
-              .filter(method => layer.route.methods[method])
-              .join(', ');
-            routeLogger.log(`Route: ${methods.toUpperCase()} - ${layer.route.path}`);
-          } else {
-            routeLogger.log('Hitting \"else\" of \"if (layer.route)\"');
-          }
-        },
-      );
-  }
 
   mainLogger.log('Calling app.useGlobalPipes()');
   app.useGlobalPipes(new ValidationPipe());
