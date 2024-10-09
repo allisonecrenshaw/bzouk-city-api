76edb42144f1845c4f672914567a948a4086032a
Allison Crenshaw <allisonecrenshaw@gmail.com>
2024-10-09 15:29:39 -0500

    Adding logic to location service to try to actually get a db record. Not yet working, needs update.

--- src/v1/location/location.controller.ts
+++ src/v1/location/location.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get, Param, Post } from '@nestjs/common';
 import { LocationService } from './location.service.js';
+import { LocationEntity } from './location.entity.js';
 
 @Controller('location')
 export class LocationController {
   constructor(private readonly locationService: LocationService) {}
 
   @Get(':id')
-  async find(@Param('id') id: string): Promise<String> {
-    return this.locationService.find(id);
+  async getById(@Param('id') id: string): Promise<LocationEntity> {
+    return this.locationService.findOneById(id);
   }
 }
--- src/v1/location/location.service.ts
+++ src/v1/location/location.service.ts
@@ -1,10 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { LocationEntity } from './location.entity.js';
 
 @Injectable()
 export class LocationService {
+  constructor(
+    @InjectRepository(LocationEntity)
+    private locationRepository: Repository<LocationEntity>,
+  ) {}
 
-  find(id: String) {
-    console.log(id);
-    return 'This will (eventually) return a location by id.';
+  async findOneById(id: string): Promise<LocationEntity> {
+    const location = await this.locationRepository.findOne({ where: { id } });
+    if (!location) {
+      throw new NotFoundException(`Location with id ${id} not found.`);
+    }
+    return location;
   }
 }
