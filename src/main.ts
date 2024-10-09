c79cafc9c1dc61675e5c1cd91842ee24ba828405
Allison Crenshaw <allisonecrenshaw@gmail.com>
2024-10-09 12:33:57 -0500

    Changing console logs to using a logger. Starting logic to log routes.

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,50 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import * as dotenv from 'dotenv';
-import { ValidationPipe } from '@nestjs/common';
-
-console.log('LOG: Starting main');
+import { ValidationPipe, Logger } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  console.log('LOG: Calling dotenv.config');
+  const mainLogger = new Logger('Main');
+  mainLogger.log('Calling dotenv.config');
   dotenv.config();
 
-  console.log('LOG: Calling await NestFactory.create(AppModule)');
+  mainLogger.log('Calling await NestFactory.create(AppModule)');
   const app = await NestFactory.create(AppModule);
-  console.log('LOG: Calling app.useGlobalPipes()');
+
+  // Get routes
+  app.enableCors();
+  const httpAdapter = app.getHttpAdapter();
+  
+  if (httpAdapter instanceof ExpressAdapter) {
+    const server = httpAdapter.getInstance();
+
+    // Logging routes
+    const routeLogger = new Logger('Routes');
+    app
+      .getHttpAdapter()
+      .getInstance()
+      ._router.stack.forEach(
+        (layer: {
+          route: { methods: { [x: string]: unknown }; path: any };
+        }) => {
+          if (layer.route) {
+            const methods = Object.keys(layer.route.methods)
+              .filter(method => layer.route.methods[method])
+              .join(', ');
+            routeLogger.log(`Route: ${methods.toUpperCase()} - ${layer.route.path}`);
+          } else {
+            routeLogger.log('Hitting \"else\" of \"if (layer.route)\"');
+          }
+        },
+      );
+  }
+
+  mainLogger.log('Calling app.useGlobalPipes()');
   app.useGlobalPipes(new ValidationPipe());
-  console.log('LOG: Calling await app.listen()');
+
+  mainLogger.log('Calling await app.listen()');
   await app.listen(3000);
 }
+
 bootstrap();
