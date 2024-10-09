fd11bd4b9d01b183b5a8333353921ce9afa267f4
Allison Crenshaw <allisonecrenshaw@gmail.com>
2024-10-08 21:46:00 -0500

    Adding logs to debug config issue.

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,17 @@ import { AppModule } from './app.module.js';
 import * as dotenv from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
 
+console.log('LOG: Starting main');
+
 async function bootstrap() {
+  console.log('LOG: Calling dotenv.config');
   dotenv.config();
 
+  console.log('LOG: Calling await NestFactory.create(AppModule)');
   const app = await NestFactory.create(AppModule);
+  console.log('LOG: Calling app.useGlobalPipes()');
   app.useGlobalPipes(new ValidationPipe());
+  console.log('LOG: Calling await app.listen()');
   await app.listen(3000);
 }
 bootstrap();
