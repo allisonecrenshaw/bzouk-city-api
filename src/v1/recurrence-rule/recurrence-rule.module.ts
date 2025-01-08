278f4d7d3bc75ecec232e754feebb71fc518501d
Allison Crenshaw <allisonecrenshaw@gmail.com>
2025-01-08 12:34:37 -0600

    Refactored all imports from v1 to be imports of those items' individual modules.

--- src/v1/recurrence-rule/recurrence-rule.module.ts
+++ src/v1/recurrence-rule/recurrence-rule.module.ts
@@ -2,11 +2,15 @@ import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RecurrenceRuleEntity } from './recurrence-rule.entity.js';
 import { RecurrenceDetailsModule } from '../recurrence-details/recurrence-details.module.js';
+import { RecurrenceRuleController } from './recurrence-rule.controller.js';
+import { RecurrenceRuleService } from './recurrence-rule.service.js';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([RecurrenceRuleEntity]),
     forwardRef(() => RecurrenceDetailsModule),
   ],
+  controllers: [RecurrenceRuleController],
+  providers: [RecurrenceRuleService],
 })
 export class RecurrenceRuleModule {}
--- src/v1/v1.module.ts
+++ src/v1/v1.module.ts
@@ -1,21 +1,13 @@
 import { Module, Logger } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { RecurrenceDetailsEntity } from './recurrence-details/recurrence-details.entity.js';
-import { RecurrenceRuleEntity } from './recurrence-rule/recurrence-rule.entity.js';
-import { RecurrenceRuleController } from './recurrence-rule/recurrence-rule.controller.js';
-import { RecurrenceRuleService } from './recurrence-rule/recurrence-rule.service.js';
 import { LocationModule } from './location/location.module.js';
+import { RecurrenceRuleModule } from './recurrence-rule/recurrence-rule.module.js';
+import { RecurrenceDetailsModule } from './recurrence-details/recurrence-details.module.js';
 
 const logger = new Logger('v1Module');
 logger.log('Hitting v1.module');
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([RecurrenceRuleEntity, RecurrenceDetailsEntity]),
-    LocationModule,
-  ],
-  controllers: [RecurrenceRuleController],
-  providers: [RecurrenceRuleService],
+  imports: [LocationModule, RecurrenceRuleModule, RecurrenceDetailsModule],
 })
 export class V1Module {}
