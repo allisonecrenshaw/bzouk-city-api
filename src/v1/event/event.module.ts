c2d4465820263e15215736c3c0d496491e1d379d
Allison Crenshaw <allisonecrenshaw@gmail.com>
2025-01-23 23:20:57 -0600

    Added EventService to EventModule.

--- src/v1/event/event.module.ts
+++ src/v1/event/event.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { EventEntity } from './event.entity.js';
 import { EventController } from './event.controller.js';
+import { EventService } from './event.service.js';
 
 @Module({
   imports: [TypeOrmModule.forFeature([EventEntity])],
   controllers: [EventController],
+  providers: [EventService],
 })
 export class EventModule {}
