66bfc9ce6d8e458619bd8a6e27044c6938e57bfe
Allison Crenshaw <allisonecrenshaw@gmail.com>
2025-01-24 20:42:18 -0600

    Added call to EventService for getById.

--- src/v1/event/event.controller.ts
+++ src/v1/event/event.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { EventService } from './event.service';
+import { EventEntity } from './event.entity';
 
 @Controller('event')
 export class EventController {
+  constructor(private readonly eventService: EventService) {}
+
   @Get(':id')
-  async getById(@Param('id') id: string): Promise<string | null> {
-    return 'temporary placeholder.';
+  // TODO Test w/ Postman
+  async getById(@Param('id') id: string): Promise<EventEntity | null> {
+    return this.eventService.findOneById(id);
   }
 }
